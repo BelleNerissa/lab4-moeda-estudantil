4a79a2746504fbde279871a2ef4a9911c141157c
BelleNerissa <belizeu@sga.pucminas.br>
2023-10-06 22:25:26 -0300

    continuacao

---
+++ Implantacao/backend/Sistema/src/main/java/br/com/pucminas/sistemamoedaestudantil/SisAplication.java
@@ -0,0 +1,14 @@
+package br.com.pucminas.sistemamoedaestudantil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisAplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisAplication.class, args);
+	}
+
+}
+
